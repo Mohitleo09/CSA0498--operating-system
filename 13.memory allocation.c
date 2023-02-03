6446292d92c8a98e90fc7574779fa26f7790891b
Mohitleo09 <114983369+Mohitleo09@users.noreply.github.com>
2023-02-03 13:59:50 +0530

    Update 13.memory allocation.c

--- 13.memory allocation.c
+++ 13.memory allocation.c
@@ -1,36 +1,24 @@
 #include<stdio.h>
-
-
 void firstFit(int blockSize[], int m, int processSize[], int n)
 {
     int i, j;
-
-
     int allocation[n];
-
-
     for(i = 0; i < n; i++)
     {
         allocation[i] = -1;
     }
-
     for (i = 0; i < n; i++)       
     {
         for (j = 0; j < m; j++)       
         {
             if (blockSize[j] >= processSize[i])
             {
-
                 allocation[i] = j;
-
-
                 blockSize[j] -= processSize[i];
-
                 break;   
             }
         }
     }
-
     printf("\nProcess No.\tProcess Size\tBlock no.\n");
     for (int i = 0; i < n; i++)
     {
@@ -43,7 +31,6 @@ void firstFit(int blockSize[], int m, int processSize[], int n)
         printf("\n");
     }
 }
-
 int main()
 {
     int m;   
@@ -52,8 +39,6 @@ int main()
     int processSize[] = {212, 417, 112, 426};
     m = sizeof(blockSize) / sizeof(blockSize[0]);
     n = sizeof(processSize) / sizeof(processSize[0]);
-
     firstFit(blockSize, m, processSize, n);
-
     return 0 ;
 }
