27650e42fc45271d2daef898cadb28aeed5127ad
Mohitleo09 <114983369+Mohitleo09@users.noreply.github.com>
2023-02-03 14:00:41 +0530

    Update 16.random access file for emplloyee details.c

--- 16.random access file for emplloyee details.c
+++ 16.random access file for emplloyee details.c
@@ -5,17 +5,13 @@ struct employee{
     int     empId;
     float   salary;
 };
-
 int main()
 {
-
     struct employee emp;
-
     printf("\nEnter details :\n");
     printf("Name ?:");          gets(emp.name);
     printf("ID ?:");            scanf("%d",&emp.empId);
     printf("Salary ?:");        scanf("%f",&emp.salary);
-
     printf("\nEntered detail is:");
     printf("Name: %s"   ,emp.name);
     printf("Id: %d"     ,emp.empId);
